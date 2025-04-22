f1fd2fbcd68298c03bba294fee3fdf3164ecc09c
KanakaA <117032244+Kanak-a@users.noreply.github.com>
2025-04-22 10:19:18 +0530

    Add files via upload

---
+++ DC/Assgn02/server.py
@@ -0,0 +1,17 @@
+import Pyro4
+
+class StringConcatenator:
+    @Pyro4.expose
+    def concatenate(self, str1, str2):
+        return str1 + str2
+
+def start_server():
+    concatenator = StringConcatenator()
+    daemon = Pyro4.Daemon()
+    uri = daemon.register(concatenator)
+    print("Server URI:", uri)
+    print("Server is ready. Waiting for requests...")
+    daemon.requestLoop()
+
+if __name__ == "__main__":
+    start_server()
